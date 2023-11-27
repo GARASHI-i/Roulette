48b9a6dac6f0f7fed5cbb9c9e32d0e4e0b6e4237
GARASHI-i <62157794+GARASHI-i@users.noreply.github.com>
2023-11-27 13:24:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// ゲームに関する変数や関数を定義
+// カードデッキの作成、カードの値の計算、ゲームの進行などを行う
 
+// デッキの作成
+const deck = [
+  // カード情報を配列に格納（例： { suit: 'ハート', value: 'A' }）
+  // 52枚のカードを作成（スートごとに13枚ずつ）
+];
+
+// ゲームの初期化（ディール、スコア計算など）
+
+// Dealボタンのクリック処理
+document.getElementById('deal-button').addEventListener('click', () => {
+  // ゲームの開始処理
+});
+
+// Hitボタンのクリック処理
+document.getElementById('hit-button').addEventListener('click', () => {
+  // プレイヤーがカードを追加する処理
+});
+
+// Standボタンのクリック処理
+document.getElementById('stand-button').addEventListener('click', () => {
+  // ディーラーの手番（カードを引く処理）と勝敗判定
+});
